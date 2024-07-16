e02f5202a965fa66588d85cd7b34cbaca52589ca
Syu-Nyakashima <san-a24040@sangi.jp>
2024-07-16 10:42:40 +0900

    0716

---
+++ Source/project01/project01.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "project01.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, project01, "project01" );
